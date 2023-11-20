92e9438a62ed7797ca57cdff316236db6074bdad
hobaoan <20520876@gm.uit.edu.vn>
2023-11-20 16:03:16 +0700

    Object detection with Yolov8s

--- Card_Detection/Presentation/ViewController.swift
+++ Card_Detection/Presentation/ViewController.swift
@@ -3,17 +3,202 @@
 //  Card_Detection
 //
 //  Created by Hồ Bảo An on 20/11/2023.
-//
+
 
 import UIKit
+import Vision
+import CoreMedia
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CameraRealtimeDelegate {
+    func updateMeasure(inferenceTime: Double, executionTime: Double, fps: Int) {
+    
+    }
+    
 
+    // MARK: - UI Properties
+
+    @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var boxesView: DrawingBoundingBoxView!
+    
+    lazy var objectDectectionModel = { return try? yolov8s() }()
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    var isInferencing = false
+    
+    // MARK: - AV Property
+    var videoCapture: VideoCapture!
+    let semaphore = DispatchSemaphore(value: 1)
+    var lastExecution = Date()
+    
+    // MARK: - TableView Data
+    var predictions: [VNRecognizedObjectObservation] = []
+    
+    // MARK - Performance Measurement Property
+    private let cameraRealtime = CameraRealtime()
+    
+    let maf1 = MovingAverageFilter()
+    let maf2 = MovingAverageFilter()
+    let maf3 = MovingAverageFilter()
+    
+    // MARK: - View Controller Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // setup the model
+        setUpModel()
+        
+        // setup camera
+        setUpCamera()
+        
+        // setup delegate for performance measurement
+        cameraRealtime.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        guard let objectDectectionModel = objectDectectionModel else { fatalError("fail to load the model") }
+        if let visionModel = try? VNCoreMLModel(for: objectDectectionModel.model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+        } else {
+            fatalError("fail to create vision model")
+        }
     }
 
-
+    // MARK: - SetUp Video
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 30
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // add preview view on the layer
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // start video preview when setup is done
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
 }
 
+// MARK: - VideoCaptureDelegate
+extension ViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?, timestamp: CMTime) {
+        // the captured image from camera is contained on pixelBuffer
+        if !self.isInferencing, let pixelBuffer = pixelBuffer {
+            self.isInferencing = true
+            
+            // start of measure
+            self.cameraRealtime.startFilming()
+            
+            // predict!
+            self.predictUsingVision(pixelBuffer: pixelBuffer)
+        }
+    }
+}
+
+extension ViewController {
+    func predictUsingVision(pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        // vision framework configures the input size of image following our model's input configuration automatically
+        self.semaphore.wait()
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        try? handler.perform([request])
+    }
+    
+    // MARK: - Post-processing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        self.cameraRealtime.tagObject(with: "endInference")
+        if let predictions = request.results as? [VNRecognizedObjectObservation] {
+//            print(predictions.first?.labels.first?.identifier ?? "nil")
+//            print(predictions.first?.labels.first?.confidence ?? -1)
+            
+            self.predictions = predictions
+            DispatchQueue.main.async {
+                self.boxesView.predictedObjects = predictions
+
+                // end of measure
+                self.cameraRealtime.stopFilming()
+                
+                self.isInferencing = false
+            }
+        } else {
+            // end of measure
+            self.cameraRealtime.stopFilming()
+            
+            self.isInferencing = false
+        }
+        self.semaphore.signal()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return predictions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell") else {
+            return UITableViewCell()
+        }
+
+        let rectString = predictions[indexPath.row].boundingBox.toString(digit: 2)
+        let confidence = predictions[indexPath.row].labels.first?.confidence ?? -1
+        let confidenceString = String(format: "%.3f", confidence/*Math.sigmoid(confidence)*/)
+        
+        cell.textLabel?.text = predictions[indexPath.row].label ?? "N/A"
+        cell.detailTextLabel?.text = "\(rectString), \(confidenceString)"
+        return cell
+    }
+}
+
+
+
+class MovingAverageFilter {
+    private var arr: [Int] = []
+    private let maxCount = 10
+    
+    public func append(element: Int) {
+        arr.append(element)
+        if arr.count > maxCount {
+            arr.removeFirst()
+        }
+    }
+    
+    public var averageValue: Int {
+        guard !arr.isEmpty else { return 0 }
+        let sum = arr.reduce(0) { $0 + $1 }
+        return Int(Double(sum) / Double(arr.count))
+    }
+}

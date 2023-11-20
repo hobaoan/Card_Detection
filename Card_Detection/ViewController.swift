e6f8f65389466d5e01355db2345cffa7a09f1e84
hobaoan <20520876@gm.uit.edu.vn>
2023-11-20 15:28:55 +0700

    Initial Commit

---
+++ Card_Detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card_Detection
+//
+//  Created by Hồ Bảo An on 20/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
